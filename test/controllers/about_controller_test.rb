10b2fc5b91c54b91290486763355cc6fdb03b6b1
Pietro Pugliesi <pietropugliesi@icloud.com>
2023-08-07 22:26:23 -0300

    master: fix testing routes assertion

--- test/controllers/about_controller_test.rb
+++ test/controllers/about_controller_test.rb
@@ -2,7 +2,12 @@ require "test_helper"
 
 class AboutControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get about_index_url
+    get about_url
     assert_response :success
   end
+
+  def test_the_truth
+    assert true
+  end
+
 end
