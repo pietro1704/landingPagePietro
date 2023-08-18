ef117e15cc606d16a169ede1d049747ec5440537
Pietro Pugliesi <pietropugliesi@icloud.com>
2023-08-18 17:05:53 -0300

    dev: fix bootstrap css precompiled bundling on prd

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin 'application', preload: true
 pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+pin 'testePips', to: 'pips.js'
 pin 'popper', to: 'popper.js', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
