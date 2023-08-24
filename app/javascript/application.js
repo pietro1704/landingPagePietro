dcd56e557fa458ce562385f6ec4d2c9d6fe17c1f
Pietro Pugliesi <pietropugliesi@icloud.com>
2023-08-24 15:53:54 -0300

    propshaft_js_css_bundling: udpated app to propshaft; with jsbundling and css bundling (bootstrap) bundlers

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,6 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails";
 import "./controllers";
+import "./pips";
 import "bootstrap";
-import "popper";
-import "testePips";
-import "controllers";
+import "@hotwired/turbo-rails";
 import "trix";
 import "@rails/actiontext";
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.public_file_server.enabled = true # enable access on production if served via puma
 
   # Compress CSS using a preprocessor.
-  config.assets.css_compressor = :scss
+  # config.assets.css_compressor = :scss
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "landingPagePietro_production"
+  # config.active_job.queue_name_prefix = "newLandingPagePietro_production"
 
   config.action_mailer.perform_caching = false
 
