fcb2c3bce7ae0bba0b697066031e6d5c2af75b11
Pietro Pugliesi <pietropugliesi@icloud.com>
2025-01-11 12:28:17 -0300

    rubocop auto lint fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-    helper_method :admin_signed_in?
-  
-    def admin_signed_in?
-      current_user && current_user.admin?
-    end
+  helper_method :admin_signed_in?
+
+  def admin_signed_in?
+    current_user&.admin?
   end
+end
